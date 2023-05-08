e4a914b0d12bb7b2dc3c96b9bba8e0a60b1f0d25
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-05-08 22:27:45 +0200

    Successfully test-drive auth flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
+
+#[tokio::main]
+async fn main() {
+    // Read application secret from a file. Sometimes it's easier to compile it directly into
+    // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
+    let secret = yup_oauth2::read_application_secret("clientsecret.json")
+        .await
+        .expect("clientsecret.json");
+
+    // Create an authenticator that uses an InstalledFlow to authenticate. The
+    // authentication tokens are persisted to a file named tokencache.json. The
+    // authenticator takes care of caching tokens to disk and refreshing tokens once
+    // they've expired.
+    let mut auth =
+        InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::HTTPRedirect)
+            .persist_tokens_to_disk("tokencache.json")
+            .build()
+            .await
+            .unwrap();
+
+    let scopes = &["https://www.googleapis.com/auth/tasks"];
+
+    // token(<scopes>) is the one important function of this crate; it does everything to
+    // obtain a token that can be sent e.g. as Bearer token.
+    match auth.token(scopes).await {
+        Ok(token) => println!("The token is {:?}", token),
+        Err(e) => println!("error: {:?}", e),
+    }
 }
